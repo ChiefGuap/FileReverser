d40e37bc4b736bacb43340bd95bfa90a03e18fcb
Raquib Alam <69777052+ChiefGuap@users.noreply.github.com>
2022-08-24 00:32:40 -0700

    updates main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,14 @@
 
 public class Main {
-
+	// this is where you can put in the filepath to the specific files that youi want to test out for the reverser
 	public static void main(String[] args) {
 		Reverser<String> strReverser = new Reverser<String>();
-		strReverser.FileToStack(//put txt file path link here);
-		strReverser.StackToFile(//put txt file path link here);
+		strReverser.FTStck(//put txt file path link here);
+		strReverser.StckTF(//put txt file path link here);
 		
 		Reverser<Float> fltReverser = new Reverser<Float>();
-		fltReverser.FileToStack(//put txt file path link here);
-		fltReverser.StackToFile(//put txt file path link here);
+		fltReverser.FTStck(//put txt file path link here);
+		fltReverser.StckTF(//put txt file path link here);
 	}
 
 }
