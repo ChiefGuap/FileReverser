a6e0f605e0d38d448cea81ca6bc563cca79b84b7
Raquib Alam <69777052+ChiefGuap@users.noreply.github.com>
2022-08-24 00:30:04 -0700

    made some updates lol

--- Reverser.java
+++ Reverser.java
@@ -5,14 +5,16 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+// Using a stack is very useful here so we can use push and pop to help with reversing contents of a file
 public class Reverser<E> {
 	private Stack<E> stck;
 
 	public Reverser() {
+		// we are creating a new stack here to read the contents of a given file 
 		stck = new Stack<E>();
 	}
-
-	public void FileToStack(String filename) {
+	// file to stack, reading everything in the file, and turning each charater into a stack, a part of an array which can be used later for reversing it
+	public void FToStck(String filename) {
 		try {
 			File file = new File(filename);
 			BufferedReader br = new BufferedReader(new FileReader(file));
@@ -20,19 +22,21 @@ public class Reverser<E> {
 			String lineRead;
 			while ((lineRead = br.readLine()) != null) {
 				lineRead = lineRead.replaceAll("\uFFFD", "\"");
+				// push means it ios where the contents of the file are being added to the stack
 				stck.push((E) lineRead);
-			}
+			} // can catch errors if file is not compatible and making sure it is readble for the comoputer. Try and catch error bound expections are thrown to make sure the code works. 
 		} catch (FileNotFoundException e) {
-			System.out.println("Error accessing the file! ");
+			System.out.println("Error when accessing file! ");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
-	public void StackToFile(String outputFile) {
+	public void StckToF(String outputFile) {
 		try {
 			PrintWriter myFileOut = new PrintWriter(outputFile);
 			while(!stck.isEmpty()) {
+				//pop is where all the contents from the stack are being pushed out charater by charater in revserve order. 
 				myFileOut.println(stck.pop());
 			}
 			myFileOut.close();
