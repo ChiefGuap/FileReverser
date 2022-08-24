1f8c16475efe79f9e5403b7c5c497a93ab57f51e
Raquib Alam <69777052+ChiefGuap@users.noreply.github.com>
2022-08-24 00:35:57 -0700

    iiii

--- Node.java
+++ Node.java
@@ -1,12 +1,16 @@
 //Class Node  ----------------------------------
+
+//after each charater the node chnages, pointing toiwards the next charater in the file 
 public class Node<E>
 {
    protected Node<E> next;
+   //this will chnage from one charater to the next, and will be pushing into the stack one by one 
    private E data;
     
    // constructor
    public Node()
    {
+      //starts out as a null pointer
       next = null;
    }
    public Node(E data)
